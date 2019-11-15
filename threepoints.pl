36e145f2cba71c539a60753fe52b240457331962
Mathew Lister <36172965+AngryPorkchop11@users.noreply.github.com>
2019-11-15 14:13:04 -0800

    Tested a little over half way

--- threepoints.pl
+++ threepoints.pl
@@ -1,6 +1,16 @@
 /* swipl -s threepoints.pl*/
 vertical(point2d(X,_Y),point2d(X,_Z)).
 horizontal(point2d(_X,Y),point2d(_Z,Y)).
+/***FORMULAS***/
+pythagorean(LHS1, LHS2, RHS):-
+  (LHS1)^2+(LHS2)^2=(RHS)^2 .
+
+pythagorean_converse_Less(LHS1, LHS2, RHS):-
+  (LHS1)^2+(LHS2)^2>(RHS)^2 .
+
+pythagorean_converse_Great(LHS1, LHS2, RHS):-
+  (LHS1)^2+(LHS2)^2<(RHS)^2 .
+
 /***LINE***/
 calculate_line(LHS1, LHS2, RHS) :-
   LHS1 + LHS2 =:= RHS.
@@ -27,14 +37,157 @@ line(point2d(X1,Y1), point2d(X2,Y2), point2d(X3,Y3)) :-
   calculate_line(AB, BC, AC).
 
 /***TRIANGLE***/
-triangle(point2d(X1,Y1), point2d(X2,Y2), point2d(X3,Y3)).
+triangle(point2d(X1,Y1), point2d(X2,Y2), point2d(X3,Y3)) :-
+  /*BC*/ XA is ((X3-X1)^2)+((Y3-Y1)^2), XA >= 0, A is sqrt(XA),
+  /*AC*/ XB is ((X3-X2)^2)+((Y3-Y2)^2), XB >= 0, B is sqrt(XB),
+  /*AB*/ XC is (((X2-X1)^2)+((Y2-Y1)^2)), XC >= 0, C is sqrt(XC),
+  P is A+B+C,
+  Semi is P/2,
+  X is Semi*(Semi - A)*(Semi - B)*(Semi - C), X >= 0, Area is sqrt(X),
+  Area =\= 0.
 
-/*
-triangle(point2d(x,y), point2d(x,y), point2d(x,y))
-isosceles(point2d(x,y), point2d(x,y), point2d(x,y))
-equilateral(point2d(x,y), point2d(x,y), point2d(x,y))
-right(point2d(x,y), point2d(x,y), point2d(x,y))
-scalene(point2d(x,y), point2d(x,y), point2d(x,y))
-acute(point2d(x,y), point2d(x,y), point2d(x,y))
-obtuse(point2d(x,y), point2d(x,y), point2d(x,y))
-*/
+/***ISOSCELES***/
+/*AB is different*/
+isosceles(point2d(X1,Y1), point2d(X2,Y2), point2d(X3,Y3)):-
+  AB is sqrt(((X2-X1)^2)+((Y2-Y1)^2)),
+  BC is sqrt(((X3-X1)^2)+((Y3-Y1)^2)),
+  AC is sqrt(((X3-X2)^2)+((Y3-Y2)^2)),
+  AB \= BC, AB \= AC, BC =:= AC.
+
+/*BC -> is different*/
+isosceles(point2d(X1,Y1), point2d(X2,Y2), point2d(X3,Y3)):-
+  AB is sqrt(((X2-X1)^2)+((Y2-Y1)^2)),
+  BC is sqrt(((X3-X1)^2)+((Y3-Y1)^2)),
+  AC is sqrt(((X3-X2)^2)+((Y3-Y2)^2)),
+  BC \= AB, BC \= AC, AB =:= AC.
+
+/*AC -> is different*/
+isosceles(point2d(X1,Y1), point2d(X2,Y2), point2d(X3,Y3)):-
+  AB is sqrt(((X2-X1)^2)+((Y2-Y1)^2)),
+  BC is sqrt(((X3-X1)^2)+((Y3-Y1)^2)),
+  AC is sqrt(((X3-X2)^2)+((Y3-Y2)^2)),
+  AC \= AB, AC \= BC, AB =:= BC.
+/*All sides are same*/
+isosceles(point2d(X1,Y1), point2d(X2,Y2), point2d(X3,Y3)):-
+  equilateral(point2d(X1,Y1), point2d(X2,Y2), point2d(X3,Y3)).
+
+/***EQUILATERAL***/
+equilateral(point2d(X1,Y1), point2d(X2,Y2), point2d(X3,Y3)):-
+  AB is sqrt(((X2-X1)^2)+((Y2-Y1)^2)),
+  BC is sqrt(((X3-X1)^2)+((Y3-Y1)^2)),
+  AC is sqrt(((X3-X2)^2)+((Y3-Y2)^2)),
+  AB =:= BC, AB =:= AC, BC =:= AC.
+
+/***RIGHT***/
+/*A -> RHS*/
+right(point2d(X1,Y1), point2d(X2,Y2), point2d(X3,Y3)):-
+  A is sqrt(((X3-X1)^2)+((Y3-Y1)^2)),
+  B is sqrt(((X3-X2)^2)+((Y3-Y2)^2)),
+  C is sqrt(((X2-X1)^2)+((Y2-Y1)^2)),
+  A > B, A > C,
+  pythagorean(B,C,A).
+
+/*B -> RHS*/
+right(point2d(X1,Y1), point2d(X2,Y2), point2d(X3,Y3)):-
+  A is sqrt(((X3-X1)^2)+((Y3-Y1)^2)),
+  B is sqrt(((X3-X2)^2)+((Y3-Y2)^2)),
+  C is sqrt(((X2-X1)^2)+((Y2-Y1)^2)),
+  B > A, B > C,
+  pythagorean(A,C,B).
+
+/*C -> RHS*/
+right(point2d(X1,Y1), point2d(X2,Y2), point2d(X3,Y3)):-
+  A is sqrt(((X3-X1)^2)+((Y3-Y1)^2)),
+  B is sqrt(((X3-X2)^2)+((Y3-Y2)^2)),
+  C is sqrt(((X2-X1)^2)+((Y2-Y1)^2)),
+  (C > A, C > B),
+  pythagorean(C,A,B).
+
+/***SCALENE***/
+scalene(point2d(X1,Y1), point2d(X2,Y2), point2d(X3,Y3)):-
+AB is sqrt(((X2-X1)^2)+((Y2-Y1)^2)),
+BC is sqrt(((X3-X1)^2)+((Y3-Y1)^2)),
+AC is sqrt(((X3-X2)^2)+((Y3-Y2)^2)),
+AB \= BC, AB \= AC, BC \= AC.
+
+/***ACUTE***/
+/*A -> RHS*/
+acute(point2d(X1,Y1), point2d(X2,Y2), point2d(X3,Y3)):-
+A is sqrt(((X3-X1)^2)+((Y3-Y1)^2)),
+B is sqrt(((X3-X2)^2)+((Y3-Y2)^2)),
+C is sqrt(((X2-X1)^2)+((Y2-Y1)^2)),
+A > B, A > C,
+pythagorean_converse_Less(B,C,A).
+
+/*==A -> RHS*/
+acute(point2d(X1,Y1), point2d(X2,Y2), point2d(X3,Y3)):-
+A is sqrt(((X3-X1)^2)+((Y3-Y1)^2)),
+B is sqrt(((X3-X2)^2)+((Y3-Y2)^2)),
+C is sqrt(((X2-X1)^2)+((Y2-Y1)^2)),
+A =:= B, C < A, C < B,
+pythagorean_converse_Less(B,C,A).
+
+/*B -> RHS*/
+acute(point2d(X1,Y1), point2d(X2,Y2), point2d(X3,Y3)):-
+A is sqrt(((X3-X1)^2)+((Y3-Y1)^2)),
+B is sqrt(((X3-X2)^2)+((Y3-Y2)^2)),
+C is sqrt(((X2-X1)^2)+((Y2-Y1)^2)),
+B > A, B > C,
+pythagorean_converse_Less(A,C,B).
+
+/*==B -> RHS*/
+acute(point2d(X1,Y1), point2d(X2,Y2), point2d(X3,Y3)):-
+A is sqrt(((X3-X1)^2)+((Y3-Y1)^2)),
+B is sqrt(((X3-X2)^2)+((Y3-Y2)^2)),
+C is sqrt(((X2-X1)^2)+((Y2-Y1)^2)),
+C =:= B, A < B, A < C,
+pythagorean_converse_Less(A,C,B).
+
+/*C -> RHS*/
+acute(point2d(X1,Y1), point2d(X2,Y2), point2d(X3,Y3)):-
+A is sqrt(((X3-X1)^2)+((Y3-Y1)^2)),
+B is sqrt(((X3-X2)^2)+((Y3-Y2)^2)),
+C is sqrt(((X2-X1)^2)+((Y2-Y1)^2)),
+C > A, C > B,
+pythagorean_converse_Less(C,A,B).
+
+/*==C -> RHS*/
+acute(point2d(X1,Y1), point2d(X2,Y2), point2d(X3,Y3)):-
+A is sqrt(((X3-X1)^2)+((Y3-Y1)^2)),
+B is sqrt(((X3-X2)^2)+((Y3-Y2)^2)),
+C is sqrt(((X2-X1)^2)+((Y2-Y1)^2)),
+A =:= C, A < B, A < C,
+pythagorean_converse_Less(C,A,B).
+
+/*All sides ==*/
+acute(point2d(X1,Y1), point2d(X2,Y2), point2d(X3,Y3)):-
+A is sqrt(((X3-X1)^2)+((Y3-Y1)^2)),
+B is sqrt(((X3-X2)^2)+((Y3-Y2)^2)),
+C is sqrt(((X2-X1)^2)+((Y2-Y1)^2)),
+A =:= C, A =:= B, B =:= C,
+pythagorean_converse_Less(C,A,B).
+
+/***OBTUSE***/
+/*A -> RHS*/
+obtuse(point2d(X1,Y1), point2d(X2,Y2), point2d(X3,Y3)):-
+A is sqrt(((X3-X1)^2)+((Y3-Y1)^2)),
+B is sqrt(((X3-X2)^2)+((Y3-Y2)^2)),
+C is sqrt(((X2-X1)^2)+((Y2-Y1)^2)),
+A > B, A > C,
+pythagorean_converse_Great(B,C,A).
+
+/*B -> RHS*/
+obtuse(point2d(X1,Y1), point2d(X2,Y2), point2d(X3,Y3)):-
+A is sqrt(((X3-X1)^2)+((Y3-Y1)^2)),
+B is sqrt(((X3-X2)^2)+((Y3-Y2)^2)),
+C is sqrt(((X2-X1)^2)+((Y2-Y1)^2)),
+B > A, B > C,
+pythagorean_converse_Great(A,C,B).
+
+/*C -> RHS*/
+obtuse(point2d(X1,Y1), point2d(X2,Y2), point2d(X3,Y3)):-
+A is sqrt(((X3-X1)^2)+((Y3-Y1)^2)),
+B is sqrt(((X3-X2)^2)+((Y3-Y2)^2)),
+C is sqrt(((X2-X1)^2)+((Y2-Y1)^2)),
+C > A, C > B,
+pythagorean_converse_Great(C,A,B).
